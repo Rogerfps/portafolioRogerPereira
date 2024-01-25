0b04bc668a2d5555f06a3fc033560f8179693171
Rogerfps <rpereira40667@ufide.ac.cr>
2024-01-24 18:35:06 -0600

    creacion de proyecto

---
+++ tienda_Roger/src/main/java/tienda/demo/TiendaRogerApplication.java
@@ -0,0 +1,13 @@
+package tienda.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaRogerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaRogerApplication.class, args);
+	}
+
+}
